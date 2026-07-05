ed6933d86f3bf439db4d22ec2f27c537b5784f7b
pushpa1011 <105695742+pushpa1011@users.noreply.github.com>
2026-07-05 15:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//your code here!
+const list = document.getElementById("list");
+const container = document.getElementById("container");
 
+let count = 1;
+
+// Function to add items
+function addItems(num) {
+    for (let i = 0; i < num; i++) {
+        const li = document.createElement("li");
+        li.textContent = "Item " + count;
+        list.appendChild(li);
+        count++;
+    }
+}
+
+// Initially add 10 items
+addItems(10);
+
+// Infinite scroll inside container
+container.addEventListener("scroll", function () {
+
+    if (
+        container.scrollTop + container.clientHeight >=
+        container.scrollHeight
+    ) {
+        addItems(2);
+    }
+
+});
